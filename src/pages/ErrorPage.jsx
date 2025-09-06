b99decc9beecd7dee025aa1c83c224782fe9aa09
shyama47 <shyamasaha588@gmail.com>
2025-09-06 14:53:11 +0600

    Implement ErrorPage (404) with back to home button

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,30 +1,30 @@
-import { Link } from "react-router";
-import ErrorImage from '../assets/error.png'
-const ErrorPage = () => {
-  return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-[#FFE66D] text-center px-6">
-      {/* Error Illustration */}
-      <img
-        src={ErrorImage}
-        alt="Error Illustration"
-        className="w-100 max-w-md my-6 rounded-2xl"
-      />
+// import { Link } from "react-router";
+// import ErrorImage from '../assets/error.png'
+// const ErrorPage = () => {
+//   return (
+//     <div className="min-h-screen flex flex-col justify-center items-center bg-[#FFE66D] text-center px-6">
+//       {/* Error Illustration */}
+//       <img
+//         src={ErrorImage}
+//         alt="Error Illustration"
+//         className="w-100 max-w-md my-6 rounded-2xl"
+//       />
 
-      {/* Error Message */}
-      <h1 className="text-4xl font-bold text-[#1A535C] mb-3">Oops! Page Not Found</h1>
-      <p className="text-gray-700 mb-6">
-        The page you are looking for doesn’t exist or has been moved.
-      </p>
+//       {/* Error Message */}
+//       <h1 className="text-4xl font-bold text-[#1A535C] mb-3">Oops! Page Not Found</h1>
+//       <p className="text-gray-700 mb-6">
+//         The page you are looking for doesn’t exist or has been moved.
+//       </p>
 
-      {/* Back to Home */}
-      <Link
-        to="/"
-        className="bg-[#4ECDC4] text-white px-6 py-3 rounded-lg hover:bg-[#FF6B6B] transition"
-      >
-        Go Back Home
-      </Link>
-    </div>
-  );
-};
+//       {/* Back to Home */}
+//       <Link
+//         to="/"
+//         className="bg-[#4ECDC4] text-white px-6 py-3 rounded-lg hover:bg-[#FF6B6B] transition"
+//       >
+//         Go Back Home
+//       </Link>
+//     </div>
+//   );
+// };
 
-export default ErrorPage;
+// export default ErrorPage;
