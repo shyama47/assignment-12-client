dd41fc347beecfe1461694b0e40680aca7bfb65f
shyama47 <shyamasaha588@gmail.com>
2025-09-12 11:24:14 +0600

    add ReportedContents page with product table, view details and delete option

--- src/pages/shared/Footer/Footer.jsx
+++ src/pages/shared/Footer/Footer.jsx
@@ -1,63 +1,63 @@
-import { NavLink } from "react-router";
-import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn } from "react-icons/fa";
+// import { NavLink } from "react-router";
+// import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn } from "react-icons/fa";
 
-const Footer = () => {
-  return (
-    <footer className="bg-[#FFE66D] text-[#1A535C] pb-5 pt-10 px-6">
-      <div className="max-w-7xl mx-auto grid grid-cols-2 md:grid-cols-4 gap-8">
+// const Footer = () => {
+//   return (
+//     <footer className="bg-[#FFE66D] text-[#1A535C] pb-5 pt-10 px-6">
+//       <div className="max-w-7xl mx-auto grid grid-cols-2 md:grid-cols-4 gap-8">
 
-        {/* Logo & Description */}
-        <div>
-          <h2 className="text-2xl font-bold mb-3">AppOrbit</h2>
-          <p className="text-sm max-w-md mx-auto -mr-16 ">
-           AppOrbit is a community-driven platform to discover, share, and review the latest tech products — including Web Apps, AI Tools, Software, Games, and Mobile Apps. Join our community to explore innovations, upvote your favorites, and showcase your own creations.
+//         {/* Logo & Description */}
+//         <div>
+//           <h2 className="text-2xl font-bold mb-3">AppOrbit</h2>
+//           <p className="text-sm max-w-md mx-auto -mr-16 ">
+//            AppOrbit is a community-driven platform to discover, share, and review the latest tech products — including Web Apps, AI Tools, Software, Games, and Mobile Apps. Join our community to explore innovations, upvote your favorites, and showcase your own creations.
 
-          </p>
-        </div>
+//           </p>
+//         </div>
 
-        {/* Menu Links */}
-        <div className="ml-20">
-          <h3 className="text-lg font-semibold mb-3 ">Menu</h3>
-          <ul className="space-y-2">
-            <li><NavLink to="/" className="hover:text-[#FF6B6B]">Home</NavLink></li>
-            <li><NavLink to="/products" className="hover:text-[#FF6B6B]">Products</NavLink></li>
-            <li><NavLink to="/login" className="hover:text-[#FF6B6B]">Login</NavLink></li>
-          </ul>
-        </div>
+//         {/* Menu Links */}
+//         <div className="ml-20">
+//           <h3 className="text-lg font-semibold mb-3 ">Menu</h3>
+//           <ul className="space-y-2">
+//             <li><NavLink to="/" className="hover:text-[#FF6B6B]">Home</NavLink></li>
+//             <li><NavLink to="/products" className="hover:text-[#FF6B6B]">Products</NavLink></li>
+//             <li><NavLink to="/login" className="hover:text-[#FF6B6B]">Login</NavLink></li>
+//           </ul>
+//         </div>
 
-        {/* Quick Links */}
-        <div>
-          <h3 className="text-lg font-semibold mb-3">Quick Links</h3>
-          <ul className="space-y-2">
-            <li><NavLink to="/contact" className="hover:text-[#FF6B6B]">Contact</NavLink></li>
-            <li><NavLink to="/terms" className="hover:text-[#FF6B6B]">Terms & Conditions</NavLink></li>
-            <li><NavLink to="/privacy" className="hover:text-[#FF6B6B]">Privacy Policy</NavLink></li>
-          </ul>
-        </div>
+//         {/* Quick Links */}
+//         <div>
+//           <h3 className="text-lg font-semibold mb-3">Quick Links</h3>
+//           <ul className="space-y-2">
+//             <li><NavLink to="/contact" className="hover:text-[#FF6B6B]">Contact</NavLink></li>
+//             <li><NavLink to="/terms" className="hover:text-[#FF6B6B]">Terms & Conditions</NavLink></li>
+//             <li><NavLink to="/privacy" className="hover:text-[#FF6B6B]">Privacy Policy</NavLink></li>
+//           </ul>
+//         </div>
 
-        {/* Social Media */}
-        <div>
-          <h3 className="text-lg font-semibold mb-3">Follow Us</h3>
-          <div className="flex space-x-4">
-            <NavLink to="/facebook" className="p-2 bg-[#1A535C] text-white rounded-full hover:bg-[#FF6B6B] transition">
-              <FaFacebookF />
-            </NavLink>
-            <NavLink to="/twitter" className="p-2 bg-[#1A535C] text-white rounded-full hover:bg-[#FF6B6B] transition">
-              <FaTwitter />
-            </NavLink>
-            <NavLink to="/linkedin" className="p-2 bg-[#1A535C] text-white rounded-full hover:bg-[#FF6B6B] transition">
-              <FaLinkedinIn />
-            </NavLink>
-          </div>
-        </div>
-      </div>
+//         {/* Social Media */}
+//         <div>
+//           <h3 className="text-lg font-semibold mb-3">Follow Us</h3>
+//           <div className="flex space-x-4">
+//             <NavLink to="/facebook" className="p-2 bg-[#1A535C] text-white rounded-full hover:bg-[#FF6B6B] transition">
+//               <FaFacebookF />
+//             </NavLink>
+//             <NavLink to="/twitter" className="p-2 bg-[#1A535C] text-white rounded-full hover:bg-[#FF6B6B] transition">
+//               <FaTwitter />
+//             </NavLink>
+//             <NavLink to="/linkedin" className="p-2 bg-[#1A535C] text-white rounded-full hover:bg-[#FF6B6B] transition">
+//               <FaLinkedinIn />
+//             </NavLink>
+//           </div>
+//         </div>
+//       </div>
 
-      {/* Bottom Copyright */}
-      <div className="mt-4 text-center text-[#1A535C] text-sm border-t border-[#1A535C]/30 pt-4">
-        © {new Date().getFullYear()} MyShop. All Rights Reserved.
-      </div>
-    </footer>
-  );
-};
+//       {/* Bottom Copyright */}
+//       <div className="mt-4 text-center text-[#1A535C] text-sm border-t border-[#1A535C]/30 pt-4">
+//         © {new Date().getFullYear()} MyShop. All Rights Reserved.
+//       </div>
+//     </footer>
+//   );
+// };
 
-export default Footer;
+// export default Footer;
